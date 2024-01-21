a1653bf331ed035efc5f3e98927ee2fa2fa23297
vitor <vitorbaraotbd@gmail.com>
2024-01-21 22:24:48 +0000

    agora sim, versao final

--- js/formulario.js
+++ js/formulario.js
@@ -1,23 +1,55 @@
-var form = document.querySelector('form');
+var form = document.querySelector('form'),
+nomeCaoAdocao = document.getElementById('nomeCaoAdocao');
+const apiUrl = 'https://api.petfinder.com/v2/animals';
+const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJvaktpdXNBSnNUdHh0RE9RRXh2c1NUVVpmczl4MlpzcHJKYWRYanZYNU5Zb1U0UjFORCIsImp0aSI6ImJiMGRiNGJiMTNlODU2YTkwMzU2YjM0ZGY2MWUxMGUwMjE2YTQ1NTI4NTNkNDU5ZWMxMjNkYzk3MzM0NzQwMTk3OWJkNTQxMjA2N2M4ZGFiIiwiaWF0IjoxNzA1ODU4ODEwLCJuYmYiOjE3MDU4NTg4MTAsImV4cCI6MTcwNTg2MjQxMCwic3ViIjoiIiwic2NvcGVzIjpbXX0.vVIhRnMMZQ8Yf9RHtc_hj-pgN0PYObYUOAXDHBT-9KCaOPR0-YDoShbXXvq6eigGNml5-0kBB4p49bcUu-108Y_4eFPuaMdEgFBKPjXiUivo3JafxJPgMQtQznxTrNejhHuOcm9uORnBkFhwyO5ILaozBrfoxvXfsHbu09mV1XcuTwyvIyD5-zUNJ7HwweF9kPEGQpYE0dOlQ6W_VJDiF_PlWQmwn2fKPguUL1fjI_DfZWK9L3-G0g34xvzaIrrUNSRPUVGo77wpkCKY9f8SQPPSfFQk0zWj9f6Xf7Ap4O18cfugD5ByWUfyPjoRtqifJO6nLByRB8_w1iHvVZPdPQ';
 
-//nao deixa dar submit ao formulario
+var nomeCao = "";
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtém o ID do animal a partir da URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const animalId = urlParams.get('id');
+
+    // Carrega os detalhes do animal com base no ID
+    carregarNomeAnimal(animalId);
+  });
+  function carregarNomeAnimal(animalId) {
+    $.ajax({
+      url: `${apiUrl}/${animalId}`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+      success: function(data) {
+        console.log(data);
+        nomeCaoAdocao.innerHTML = "Adotar " + data.animal.name;
+        nomeCao = data.animal.name;
+      },
+      error: function(error) {
+        window.alert("Ups, não existe o  cão que quer adotar");
+        window.location.href = `../adocao.html`;
+        console.error('Erro ao obter detalhes do animal da PetFinder:', error);
+      }
+    });
+} 
+//nao deixa dar submit ao formulario enquanto o formo não for validado
 form.addEventListener('submit', function (event) {
     //se retornar falso, nao esta validado o form
-    if (!validateForm()) {
-        event.preventDefault();
+    if (!validarForm()) {
+        return event.preventDefault();
     }
+    addCaoAdotado(nomeCao);
 });
-
-function validateForm(){
+function validarForm() {
     //vai buscar todos as caixas de texto
     let inputs = form.getElementsByTagName("input"), input = null, validacao = true;
 
     //percorre todos os inputs
-    for(let i = 0, len = inputs.length; i < len; i++) {
+    for (let i = 0, len = inputs.length; i < len; i++) {
         //referencia o input atual
         input = inputs[i];
         //se o input nao esta validado, erro
-        if(!input.value) {
+        if (!input.value) {
             validacao = false;
             input.focus();
             //vai dar scroll para o input não validado
@@ -26,5 +58,22 @@ function validateForm(){
             break;
         }
     }
-    return(validacao);
+    return (validacao);
+}
+function activeModal() {
+    const content_modal = document.getElementById('modal');
+    content_modal.classList.toggle('active');
+}
+function addCaoAdotado(nomeCao) {
+    if (typeof (Storage) !== "undefined") {
+        var nomeCao_array = JSON.parse(localStorage.getItem("nomeCao")) || [];
+        nomeCao_array.push(nomeCao);
+        localStorage.setItem("nomeCao", JSON.stringify(nomeCao_array));
+        window.alert("Foi adotado com sucesso");
+    }
+    else console.log("Nao suporta cookies")
+
+}
+function voltarPaginaAdocao(){
+    window.location.href = `../adocao.html`;
 }
